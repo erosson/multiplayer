d240ccd3f5abac50f740df9f74f58f0bc72c72d6
Evan Rosson <git@erosson.org>
2023-01-17 05:13:14 +0000

    kubectl

---
+++ vendor/install-protoc.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+set -eux
+cd "`dirname "$0"`"
+# https://github.com/protocolbuffers/protobuf/releases/latest
+wget -O protoc.zip https://github.com/protocolbuffers/protobuf/releases/download/v21.12/protoc-21.12-linux-x86_64.zip
+rm -rf protoc
+unzip -d protoc *.zip
