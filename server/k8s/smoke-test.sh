b3d87386eeef9ffb03b3ecfe55dce2399241ce58
Evan Rosson <git@erosson.org>
2023-01-19 21:05:36 +0000

    k8s www deployment (not yet exposed); refactor k8s stuff

--- server/k8s/smoke-test.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# TODO run with jest, or some other test framework
-set -eux
-
-curl http://multiplayer-test.erosson.org:3000/
-curl http://multiplayer-test3.erosson.org:30000/
-echo 'pass'
