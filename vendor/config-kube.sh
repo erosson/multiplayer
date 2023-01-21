dad01971b4297c201b18638cbdde8d9c5a5b2739
Evan Rosson <git@erosson.org>
2023-01-21 07:32:47 +0000

    remove k8s stuff

--- vendor/config-kube.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-# populate `~/.kube/config` from `$KUBE_CONFIG`. this is a secret set in github-actions and gitpod. It's secret.
-# feels like the default file should be named `.kube/konfig`...
-set -eu
-mkdir -p $HOME/.kube
-echo $KUBE_CONFIG | base64 -di > $HOME/.kube/config
