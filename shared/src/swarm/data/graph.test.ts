62f8e33bca5b828b1413429ef6827ef8a5c22e0c
Evan Rosson <git@erosson.org>
2023-01-27 02:30:35 +0000

    fix test

--- shared/src/swarm/data/graph.test.ts
+++ shared/src/swarm/data/graph.test.ts
@@ -1,7 +1,9 @@
 import * as G from "./graph";
+import * as D from "./index";
 
 test("prod graph", () => {
-  const g = G.producer();
+  const d = D.create();
+  const g = G.producer(d.unit.list);
   expect(g).toBeTruthy();
   // are paths built? are they going the right direction?
   expect(Math.max(...g.childPaths.larva.map((p) => p.path.length))).toBe(1);
